052e24809bf053a3377215fce061bd05d9eece71
Maria Choo <mchoo.1942@gmail.com>
2018-05-08 20:54:12 +1000

    changed player

--- Player.py
+++ Player.py
@@ -12,6 +12,7 @@ class Player:
     killerMoves = None
     bestMove = None
     bestScore = -float('inf')
+    depth = 3
 
 
     def __init__(self, colour):
@@ -23,41 +24,46 @@ class Player:
             self.enemyPiece = "W"
 
         self.currentBoard = Board(self.BOARD_SIZE)
-        # self.killerMoves = [2][3]
+        self.killerMoves = [[None for x in range(1)] for x in range(self.depth)]
 
-        self.currentBoard.addPiece( 3, 2, "B")
-        self.currentBoard.addPiece( 3, 3, "B")
-        self.currentBoard.addPiece( 3, 5, "B")
-        self.currentBoard.addPiece( 2, 2, "B")
-        self.currentBoard.addPiece( 2, 4, "B")
-        self.currentBoard.addPiece( 1, 0, "B")
-        self.currentBoard.addPiece( 0, 2, "W")
+
+        self.currentBoard.addPiece(3, 2, "B")
+        self.currentBoard.addPiece(3, 3, "B")
+        self.currentBoard.addPiece(3, 5, "B")
+        self.currentBoard.addPiece(2, 2, "B")
+        self.currentBoard.addPiece(2, 4, "B")
+        self.currentBoard.addPiece(1, 0, "B")
+        self.currentBoard.addPiece(0, 2, "W")
         self.currentBoard.__str__()
 
-        # self.ABRoot(self.currentBoard, 3, self.playerPiece)
-        # self.ABPruning(self.currentBoard, 4, -float('inf'), float('inf'), self.playerPiece)
-
-        # self.currentBoard._shrink_board()
-        # self.currentBoard._shrink_board()
 
     def action(self, turns):
         if self.phase == "placing" and turns > self.PLACING_PHASE_MOVES:
             self.phase == "moving"
-        depth = 3
-        blank, move = self.ABPruning(self.currentBoard, depth, -float('inf'), float('inf'), self.playerPiece)
+
+        elif turns in [128, 192]:
+            self.currentBoard._shrink_board()
+
+        blank, move = self.ABPruning(self.currentBoard, self.depth, -float('inf'), float('inf'), self.playerPiece)
+
         if type(move[0]) is tuple:
             self.currentBoard.movePiece(move[0], move[1])
         elif type(move[0]) is int:
-            self.currentBoard.addPiece( move[0], move[1], self.playerPiece)
+            self.currentBoard.addPiece(move[0], move[1], self.playerPiece)
+
         self.currentBoard.__str__()
+        print(self.currentBoard.pieces)
+
+        return move
 
 
     def update(self, action):
         if type(action[0]) is tuple:
             self.currentBoard.movePiece(action[0], action[1])
         elif type(action[0]) is int:
-            self.currentBoard.addPiece(action[0], action[1], self.enemyPiece, )
+            self.currentBoard.addPiece(action[0], action[1], self.enemyPiece)
         self.currentBoard.__str__()
+
 
 
     def ABPruning(self, board, depth, a, b, player):
@@ -67,48 +73,50 @@ class Player:
         bestMove = None
         bestVal = -float('inf')
 
-        if player == self.playerPiece:
-            value = -float('inf')
+        if self.phase == "placing":
+            moves = board.getPossiblePiecePlaces()
+        else:
+            moves = board.findAllMoves(player)
+
+        # Killer heuristic
+        for slot in range(0, len(self.killerMoves[depth-1])-1):
+            killerMove = self.killerMoves[depth-1][slot]
+            for m in range(0, len(moves)):
+                if m == killerMove:
+                    self.addKillerMove(depth-1, m)
+                    break;
+
+        killerMoves = []
+        for killerMove in self.killerMoves[depth -1]:
+            if killerMove is not None:
+               killerMoves.append(killerMove)
+
+        moves = killerMoves + moves
+
+        for move in moves:
+            newBoard = deepcopy(board)
 
             if self.phase == "placing":
-                moves = board.getPossiblePiecePlaces()
+                newBoard.addPiece(move[0], move[1], player)
             else:
-                moves = board.checkMoves(player)
+                newBoard.movePiece(move[0], move[1])
 
-            for move in moves:
-                newBoard = deepcopy(board)
-
-                if self.phase == "placing":
-                    newBoard.addPiece(*move, player)
-                else:
-                    newBoard.movePiece(*move)
+            if player == self.playerPiece:
+                value = -float('inf')
 
                 value = max(value, self.ABPruning(newBoard, depth - 1, a, b, self.enemyPiece)[0])
                 # Hard-coded for now
-                if value > bestVal and depth == 3:
+                if value > bestVal and depth == self.depth:
                     bestVal = value
                     bestMove = move
                     print(bestMove)
                 a = max(a, value)
                 if b <= a:
+                    self.addKillerMove(depth-1, move)
                     break;
 
-        else:
-            value = float('inf')
-
-            if self.phase == "placing":
-                moves = board.getPossiblePiecePlaces()
             else:
-                moves = board.checkMoves(player)
-
-            for move in moves:
-                newBoard = deepcopy(board)
-
-                if self.phase == "placing":
-                    newBoard.addPiece( move[0], move[1], player,)
-                else:
-                    newBoard.movePiece(move[0], move[1])
-
+                value = float('inf')
                 value = min(value, self.ABPruning(newBoard, depth - 1, a, b, self.playerPiece)[0])
                 b = min (b, value)
                 if b <= a:
@@ -116,6 +124,10 @@ class Player:
 
         return (value, bestMove)
 
+    def addKillerMove(self, depth, move):
+        for i in range(len(self.killerMoves[depth]) - 2, -1, -1):
+            self.killerMoves[depth][i + 1] = self.killerMoves[depth][i]
+        self.killerMoves[depth][0] = move
 
 t = Player("white")
 t.action(1)
